0da33ad6bc1e402a00c7834a313048a75c8e1a03
Mohammed <i23281@verisk.com>
2024-11-01 22:13:51 -0400

    tools-setup-code -  Terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "tools" {
   source = "./modules-ec2"
   tool_name= each.key
   sg_port=each.value["port"]
-  volume_size=each.value["volumae_size"]
+  volume_size=each.value["volume_size"]
   instance_type = each.value["instance_type"]
   domain_name = var.domain_name
   zone_id=var.zone_id
