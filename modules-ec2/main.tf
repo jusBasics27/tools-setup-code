4098a8e310534a4d1bca94152a878a83b4125ae1
Mohammed <i23281@verisk.com>
2025-01-14 03:10:09 -0500

    filebeat

--- modules-ec2/main.tf
+++ modules-ec2/main.tf
@@ -10,11 +10,22 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port        = var.sg_port
-    to_port          = var.sg_port
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
+  # ingress {
+  #   from_port        = var.sg_port
+  #   to_port          = var.sg_port
+  #   protocol         = "TCP"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # }
+
+  dynamic "ingress" {
+    for_each = var.sg_port
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = ingress.key
+    }
   }
 
 
